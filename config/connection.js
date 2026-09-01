95a9d7dd4c1db6d57221dd109de6222882f28c16
Ashbin Shaji <appuminnu500@gmail.com>
2026-09-01 21:54:45 +0530

    fix mongodb atlas connection

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,7 @@ module.exports.connect = function (done) {
         return done(new Error("DB_PASSWORD is missing"));
     }
 
-    const url = `mongodb://ashbinshaji20088_db_user:${dbPassword}@ac-swqdy4r-shard-00-00.kk65zx8.mongodb.net:27017,ac-swqdy4r-shard-00-01.kk65zx8.mongodb.net:27017,ac-swqdy4r-shard-00-02.kk65zx8.mongodb.net:27017/?ssl=true&replicaSet=atlas-d1rnyv-shard-0&authSource=admin&appName=Cluster0`;
+    const url = process.env.MONGODB_URI;
 
     console.log("Trying to connect..");
 
